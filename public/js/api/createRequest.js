e59aa3be3293fb4488bdad5b2a9fbaafdf9a9e90
RebikHub <rebikovlk@mail.ru>
2021-07-04 18:05:48 +0700

    begin

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,28 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
+const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
+    options.callback = (err, response) => {
+        try {
+            xhr.onload = function() {
+                callback(null, xhr.response);
+            };
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    xhr.addEventListener('load', options.callback);
+    if (options.method === 'GET') {
+        xhr.open(options.method, options.url);
+        xhr.send();
+    } else {
+        let formData = new FormData;
+        formData.append('mail', optins.data.mail);
+        formData.append('password', options.data.password);
+        xhr.open(optins.method, options.url);
+        xhr.send(formData);
+    };
+    xhr.responseType = 'json';
 };
